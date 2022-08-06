a69d3d93e1b615f39d2584442e983df736116766
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-08-05 23:50:38 -0300

    :pencil: Adiciona mais parâmetros à api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,2 +1,6 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-export const key = "d66b30580c1005ed0f66d2c1486bb40d"
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+export const BASE_URL_DAY = "https://api.openweathermap.org/data/2.5/forecast?"
+export const KEY = "d66b30580c1005ed0f66d2c1486bb40d"
+export const ICON = "http://openweathermap.org/img/w"
+export const UNIT_TEMPERATURE = "units=metric"
+export const LANG = "lang=pt_br"
